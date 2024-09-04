15d6af3d7e93c2dec54f46fe8e68a7a421c91517
reztib <bitzer2@gmx.de>
2024-09-04 19:16:47 +0200

    Verbesserung der UI/UX der Login- und Upload-Seiten: Zentriertes Layout, optimierte Typografie, responsives Design hinzugefügt, visuelles Feedback für Eingabefelder und Buttons integriert, und Fehlermeldungen für bessere Benutzererfahrung gestylt

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 session_start();
 
-// Überprüfen, ob der Benutzer bereits eingeloggt ist
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
-    header('Location: upload.html'); // Weiterleitung zur Upload-Seite, wenn bereits eingeloggt
+    header('Location: upload.html');
     exit;
 }
 ?>
@@ -17,20 +16,20 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <h2>Login</h2>
-    <form action="authenticate.php" method="post">
-        <label for="username">Benutzername:</label>
-        <input type="text" id="username" name="username" required><br><br>
-        <label for="password">Passwort:</label>
-        <input type="password" id="password" name="password" required><br><br>
-        <input type="submit" value="Anmelden">
-    </form>
-
-    <?php
-    // Zeigt eine Fehlermeldung an, wenn der Fehlerparameter in der URL vorhanden ist
-    if (isset($_GET['error']) && $_GET['error'] == '1') {
-        echo '<p style="color:red;">Ungültiger Benutzername oder Passwort.</p>';
-    }
-    ?>
+    <div class="container">
+        <h2>Login</h2>
+        <form action="authenticate.php" method="post">
+            <label for="username">Benutzername:</label>
+            <input type="text" id="username" name="username" required>
+            <label for="password">Passwort:</label>
+            <input type="password" id="password" name="password" required>
+            <input type="submit" value="Anmelden">
+        </form>
+        <?php
+        if (isset($_GET['error']) && $_GET['error'] == '1') {
+            echo '<p class="error-messages">Ungültiger Benutzername oder Passwort.</p>';
+        }
+        ?>
+    </div>
 </body>
 </html>
